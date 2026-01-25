c014549e4e436e120fa5e83ec720e26053c7efa0
Gaganpreet Kaur <gkaur21@academic.rrc.ca>
2026-01-24 20:29:27 -0600

    Added my employeeForm component and modified app.tsx file by using state

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,51 @@ import HeaderH1 from "../src/components/page/header";
 import Paragraph from "./components/page/paragraph";
 import Footer from "./components/page/footer";
 import Section from "./components/page/section";
-import Main from "./components/page/main";
 import './components/page/style.css';
+import { initialEmployees } from "./data/data";
+import EmployeeForm from "./components/page/employeeForm";
+import { useState } from "react";
+
 
 function App() {
+  // This is my state where I am using flatMap to convert my data into list.
+const [employees, setEmployees] = useState(initialEmployees.flatMap(dept =>
+    dept.employees.map(emp => ({
+      firstName: emp.firstName,
+      department: dept.name
+    }))
+  )
+);
+
+
+const addEmployee = (employee: any) => {
+  setEmployees([...employees, employee]);
+};
+
 
   return (
     <>
     <HeaderH1 />
     <Paragraph />
     <Section />
-    <Main />
+{["Health Care", "Technology", "Finance"].map((dept) => (
+  <div key={dept}>
+    <h2>{dept}</h2>
+    <ul>
+      {employees
+        .filter(emp => emp.department === dept && emp.firstName)
+        .map(emp => (
+          <li key={emp.firstName}>{emp.firstName}</li>
+        ))}
+      </ul>
+  </div>
+))}
+
+    <EmployeeForm addEmployee={addEmployee} />
+
     <Footer />
     </>
   );
 }
 
 export default App;
-
---
+++ src/data/data.ts
@@ -0,0 +1,25 @@
+import type { Department } from "../interface/Department"
+
+export const initialEmployees: Department[] = [
+  {
+    "name": "Health Care",
+    "employees": [
+      { "firstName": "Gaganpreet", "lastName": "Kaur" },
+      { "firstName": "Parampreet", "lastName": "Singh" }
+    ]
+  },
+  {
+    "name": "Technology",
+    "employees": [
+      { "firstName": "Anand", "lastName": "Kaur" },
+      { "firstName": "Sahib", "lastName": "Singh" }
+    ]
+  },
+  {
+    "name": "Finance",
+    "employees": [
+      { "firstName": "Manjot", "lastName": "Kaur" },
+      { "firstName": "Harmanpreet", "lastName": "Khosa" }
+    ]
+  }
+]
